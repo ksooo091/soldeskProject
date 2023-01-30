601715a520045579309b54717a5c16b4f19a4c70
KimGilSu <hwanrim00@gmail.com>
2023-01-30 17:06:11 +0900

    [feat] iamuser

--- terraform/iam/iamGroup.tf
+++ terraform/iam/iamGroup.tf
@@ -1,3 +1,22 @@
-# resource "aws_iam_group" ""{
-#  name = ""
-# }
+resource "aws_iam_group" "eks_group"{
+ name = "eksGroup"
+}
+
+
+resource "aws_iam_group_policy_attachment" "eks_group_attach" {
+  group      = aws_iam_group.eks_group.name
+  policy_arn =  "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_group_membership" "eksteam" {
+  name = "eks-group-membership"
+
+  users = [
+    aws_iam_user.user_kiwon.name,
+    aws_iam_user.user_joonpyo.name,
+    aws_iam_user.user_jinha.name,
+    aws_iam_user.user_gilsu.name,
+  ]
+
+  group = aws_iam_group.eks_group.name
+}
--- terraform/iam/iamuser.tf
+++ terraform/iam/iamuser.tf
@@ -1,15 +1,64 @@
-resource "aws_iam_user" "eks_user"{
-    name = "eks_user"
-    tags = {
-      "temauser" = "name"
-    }
+resource "aws_iam_user" "user_kiwon"{
+    name = "userkiwon"
+    # tags = {
+    #   "temauser" = "name"
+    # }
 }
 
+resource "aws_iam_user" "user_joonpyo"{
+    name = "userjoonpyo"
+    # tags = {
+    #   "temauser" = "name"
+    # }
+}
 
+resource "aws_iam_user" "user_jinha"{
+    name = "userjinha"
+    # tags = {
+    #   "temauser" = "name"
+    # }
+}
+resource "aws_iam_user" "user_gilsu"{
+    name = "usergilsu"
+    # tags = {
+    #   "temauser" = "name"
+    # }
+}
 
-resource "aws_iam_policy_attachment" "test-attach" {
-  name       = "test-attachment"
-  users      = [aws_iam_user.eks_user.name]
-  policy_arn =  "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+resource "aws_iam_access_key" "user_kiwon" {
+  user    = aws_iam_user.user_kiwon.name
+}
 
+resource "aws_iam_access_key" "user_joonpyo" {
+  user    = aws_iam_user.user_joonpyo.name
+}
+
+resource "aws_iam_access_key" "user_jinha" {
+  user    = aws_iam_user.user_jinha.name
+}
+
+resource "aws_iam_access_key" "user_gilsu" {
+  user    = aws_iam_user.user_gilsu.name
+}
+
+resource "aws_iam_user_login_profile" "user_gilsu" {
+  user    = aws_iam_user.user_gilsu.name
+  password_reset_required = true
+}
+
+resource "aws_iam_user_login_profile" "user_jinha" {
+  user    = aws_iam_user.user_jinha.name
+  password_reset_required = true
+}
+resource "aws_iam_user_login_profile" "user_joonpyo" {
+  user    = aws_iam_user.user_joonpyo.name
+  password_reset_required = true
+}
+resource "aws_iam_user_login_profile" "user_kiwon" {
+  user    = aws_iam_user.user_kiwon.name
+  password_reset_required = true
+}
+
+output "password" {
+  value = aws_iam_user_login_profile.user_gilsu.encrypted_password
 }
