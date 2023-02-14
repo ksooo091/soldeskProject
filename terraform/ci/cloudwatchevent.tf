afec6b77cbb543c3681bcc8642169ea7784d8427
ksooo091 <hwanrim00@gmail.com>
2023-02-15 00:42:51 +0900

    cwerole

--- terraform/ci/cloudwatchevent.tf
+++ terraform/ci/cloudwatchevent.tf
@@ -1,12 +1,13 @@
 
-resource "aws_codecommit_trigger" "front_trigger" {
-  repository_name = aws_codecommit_repository.frontend-server.repository_name
-}
+# resource "aws_codecommit_trigger" "front_trigger" {
+#   repository_name = aws_codecommit_repository.frontend-server.repository_name
+# }
 
 resource "aws_cloudwatch_event_target" "front_target" {
   target_id = "frontpipe-target"
-  rule      = aws_cloudwatch_event_rule.console.name
+  rule      = aws_cloudwatch_event_rule.front_watch_rule.name
   arn       = aws_codepipeline.front_pipeline.arn
+  role_arn = aws_iam_role.codepipeline_role.arn  
 
 }
 
@@ -14,29 +15,36 @@ resource "aws_cloudwatch_event_rule" "front_watch_rule" {
   name        = "front-watch-rule"
   description = "front code repo watch rule"
 
-  event_pattern = <<PATTERN
-{
-  "source": [
-    "aws.codecommit"
-  ],
-  "detail-type": [
-    "CodeCommit Repository State Change"
-  ],
-  "resources": [
-    ${aws_codecommit_repository.frontend-server.arn}
-  ],
-  "detail": {
-    "event": [
-      "referenceCreated",
-      "referenceUpdated"
+  event_pattern = jsonencode(
+  {
+    "source": [
+      "aws.codecommit"
     ],
-    "referenceType": [
-      "branch"
+    "detail-type": [
+      "CodeCommit Repository State Change"
     ],
-    "referenceName": [
-      "main"
-    ]
-  }
+    "resources": [
+      aws_codecommit_repository.frontend-server.arn
+    ],
+    "detail": {
+      "event": [
+        "referenceCreated",
+        "referenceUpdated"
+      ],
+      "referenceType": [
+        "branch"
+      ],
+      "referenceName": [
+        "main"
+      ]
+    }
+  })
 }
-PATTERN
+
+resource "aws_iam_role" "cwe_role" {
+  name = "crw-role-repo"
+  assume_role_policy = <<EOF
+  
+
+
 }
